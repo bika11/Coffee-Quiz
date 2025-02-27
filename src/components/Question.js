73c417dc07da0a7f120620ef96b46106049c6c15
bika11 <bikicole@gmail.com>
2025-02-27 22:42:28 +0000

    refactor: Update Question component and QuizContainer to use nested data structure

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,1 +1,33 @@
-// Question.js
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Question({ question }) {
+  return (
+    <div>
+      <h2>{question.question_text}</h2>
+      <ul>
+        {question.answers.map((answer) => (
+          <li key={answer.answer_id}>{answer.answer_text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Question.propTypes = {
+  question: PropTypes.shape({
+    question_id: PropTypes.number.isRequired,
+    question_text: PropTypes.string.isRequired,
+    mode: PropTypes.string.isRequired,
+    sort_order: PropTypes.number.isRequired,
+    answers: PropTypes.arrayOf(
+      PropTypes.shape({
+        answer_id: PropTypes.number.isRequired,
+        answer_text: PropTypes.string.isRequired,
+        sort_order: PropTypes.number.isRequired,
+      })
+    ).isRequired,
+  }).isRequired,
+};
+
+export default Question;
--- src/components/QuizContainer.js
+++ src/components/QuizContainer.js
@@ -1,1 +1,11 @@
-// QuizContainer.js
+import React from 'react';
+
+function QuizContainer() {
+  return (
+    <div>
+      <h1>Quiz Container</h1>
+    </div>
+  );
+}
+
+export default QuizContainer;
